81a1b5b3ccb142417994698d76687a979ba0ae4b
iamhabi <iamhabi@pm.me>
2024-04-18 09:49:10 +0900

    loop scrap on background thread

--- scrap/src/lib.rs
+++ scrap/src/lib.rs
@@ -1,5 +1,17 @@
 pub mod scrap;
 
+pub fn loop_scrap() {
+    std::thread::spawn(|| {
+        let delay_time = std::time::Duration::from_millis(1000 * 60 * 60);
+  
+        loop {
+            scrap();
+    
+            std::thread::sleep(delay_time);
+        }
+    });
+}
+
 pub fn scrap() {
     scrap::scrap_news();
 }
