721701cf7b14e0d6189ded75fd1a25a0af403e9c
iamhabi <iamhabi@pm.me>
2024-04-24 13:26:30 +0900

    news list as json

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::Json;
 use rocket_dyn_templates::Template;
 
 use std::collections::HashMap;
@@ -8,9 +9,12 @@ use std::collections::HashMap;
 use sql;
 use scrap;
 
-#[get("/")]
-async fn home() -> Template {
-    let news = sql::get_news();
+#[get("/?<limit>&<offset>")]
+async fn home(limit: Option<i64>, offset: Option<i64>) -> Template {
+    let limit = limit.unwrap_or(100);
+    let offset = offset.unwrap_or(0);
+
+    let news = sql::get_news(limit, offset);
 
     let mut map = HashMap::new();
     map.insert("news", news);
@@ -18,11 +22,21 @@ async fn home() -> Template {
     Template::render("home", map)
 }
 
+#[get("/json?<limit>&<offset>")]
+pub fn get_news_as_json(limit: Option<i64>, offset: Option<i64>) -> Json<Vec<sql::models::News>> {
+    let limit = limit.unwrap_or(100);
+    let offset = offset.unwrap_or(0);
+
+    let news = sql::get_news(limit, offset);
+
+    Json(news)
+}
+
 #[rocket::launch]
 pub fn rocket() -> _ {
     scrap::loop_scrap();
 
     rocket::build()
-        .mount("/", routes![home])
+        .mount("/", routes![home, get_news_as_json])
         .attach(Template::fairing())
 }
