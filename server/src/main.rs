28d8dc615d90b341856e4f441d64f38d62f25328
iamhabi <iamhabi@pm.me>
2024-04-25 12:25:46 +0900

    news with page count

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
+use serde::{Serialize, Deserialize};
 use rocket::serde::json::Json;
+use rocket::fs::{ FileServer, relative };
 use rocket_dyn_templates::Template;
 
 use std::collections::HashMap;
@@ -9,15 +11,30 @@ use std::collections::HashMap;
 use sql;
 use scrap;
 
-#[get("/?<count>&<offset>")]
-async fn home(count: Option<i64>, offset: Option<i64>) -> Template {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Data {
+    pub news: Vec<sql::models::News>,
+    pub page_count: i64
+}
+
+#[get("/?<count>&<page>")]
+async fn home(count: Option<i64>, page: Option<i64>) -> Template {
     let count = count.unwrap_or(100);
-    let offset = offset.unwrap_or(0);
+    let page = page.unwrap_or(1);
+
+    let offset = count * (page - 1);
 
     let news = sql::get_news(count, offset);
+    let page_count = sql::get_count() / count;
 
     let mut map = HashMap::new();
-    map.insert("news", news);
+
+    let data = Data {
+        news,
+        page_count
+    };
+
+    map.insert("data", data);
 
     Template::render("home", map)
 }
@@ -43,5 +60,6 @@ pub fn rocket() -> _ {
 
     rocket::build()
         .mount("/", routes![home, get_news_as_json, get_count])
+        .mount("/static", FileServer::from(relative!("static")))
         .attach(Template::fairing())
 }
